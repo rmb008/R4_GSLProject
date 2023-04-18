9df709c405a53835880c2536f0c8209f2b299230
Vanessa <vama3024@colorado.edu>
2023-04-18 12:00:53 -0600

    calculuated more values like R^2, started detecting problems in regression but need to find a way to fix still

--- regression.py
+++ regression.py
@@ -6,11 +6,12 @@ import scipy as sp
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
+import seaborn as sns
 
 lmdata = pd.read_csv("field_data.csv")
 #print(lmdata)
 
-# replace first header (need second header to pull our needed columns
+# replace first header (need second header to pull our needed columns)
 new_header = lmdata.iloc[0]
 lmdata = lmdata[1:]
 lmdata.columns = new_header
@@ -19,7 +20,7 @@ lmdata.columns = new_header
 # remove last row (it's the sum of all previous rows for each column)
 lmdata = lmdata.drop(labels=157, axis=0)
 
-# select the columns we need (note: the csv file has an extra space at the end of the temp string
+# select the columns we need (note: the csv file has an extra space at the end of the temp string)
 lmdata_reduced = lmdata[["SALINITY (%)", "TEMPERATURE (C) ", "CHLOROPHYTES (ug/l)"]]
 #print(lmdata_reduced)
 
@@ -34,7 +35,8 @@ lmdata_filter3 = lmdata_filter2[lmdata_filter2["CHLOROPHYTES (ug/l)"] != '"99999
 # need to re-index this for regression
 lmdata = lmdata_filter3.reset_index()
 #print(lmdata)
-# drop extra index column
+
+# drop extra index column from re-indexing
 lmdata = lmdata.drop(labels="index", axis=1)
 #print(lmdata)
 
@@ -44,10 +46,37 @@ Y = lmdata["CHLOROPHYTES (ug/l)"]
 
 # run the regression using sklearn
 regr = linear_model.LinearRegression()
-regr.fit(X, Y)
+model = regr.fit(X, Y)
 
 # print parameters/coefficients
-print(regr.coef_)
+#print(regr.coef_)
+
+# print intercept of fit line
+#print(regr.intercept_)
+
+# print R^2
+#print(model.score(X, Y)) # R^2 is currently low, possibly poor model fit (TODO: test for correlation between temp and salinity: should not be correlated)
+
+# plot SLR's
+plt.scatter(X["SALINITY (%)"], Y) # salinity and chlorophytes
+#plt.show()
+plt.scatter(X["TEMPERATURE (C) "], Y) # temp and chlorophytes
+#plt.show()
+
+# plot temp vs salinity (proxy test for correlation, official correlation test coming) - almost linear: PROBLEM
+plt.scatter(X["SALINITY (%)"], X["TEMPERATURE (C) "])
+#plt.show()
+
+# remove 0's from Y as test (to see impact on regression - then incorporate advice from zero-inflated regression article)
+Y_nozeros = Y[Y != "0"] # note that the values in lmdata are strings: TODO: see if this is a problem
+print(Y)
+print(Y_nozeros)
+
+# TODO: deal with zero's: are these the problem or is it something else (e.g. multicollinearity?)
+
+# TODO: run diagnostic plots
+
+# TODO: 3D plots of MLR
 
 # TODO: need to split data first and figure out how to plot MLR - want to get a plot and estimate of its goodness before tomorrow
 
@@ -56,5 +85,3 @@ print(regr.coef_)
 # taken from same place and depth too)
 
 # TODO: don't forget to split into testing and training sets before predicting, calculuate MSPE, etc
-# TODO: merge environments or rename or something (currently, this works with sklearn env but needs to be a unique env)-
-# currently using sklearn env (I think that is the right one, and it is connected to the environment.yml file)
