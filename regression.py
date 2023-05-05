6c364a0fdf893228b883d8bb41050bbc0d3e7896
Vanessa <vama3024@colorado.edu>
2023-05-05 00:02:44 -0600

    computed prediction and error values

--- regression.py
+++ regression.py
@@ -14,31 +14,27 @@ from sklearn.model_selection import train_test_split
 from patsy import dmatrices
 import statsmodels.api as sm
 import statsmodels
-#from statsmodels import statsmodels.discrete.discrete_model
+import math
+import random
+#from statsmodels import statsmodels.discrete.count_model
 
 #################################################################################################
 # TODO LIST:
 
+# - TODO: we get same ZIP results each time for same data; how much should ZIP results be changing for diff data?? Currently have it set to same data each time - need to bootstrap
+# - TODO: is 7.5% right? how to get associated increase in salinity? IT's HALFWAY IN BETWEEN EXPECTED CLIMATE CHANGE SCENARIO FOR BOTH TEMP AND SAL
 # - TODO: deal with negative fitted values
-# - TODO: fit nonparametric lines (esp to diagnostic plots)
-# - TODO: run diagnostic plots and test goodness of fit (esp for ZIP)
-# - TODO: beautify figures
-# - TODO: calculate residuals and/or error
+# - TODO: run more diagnostic plots and test goodness of fit (esp for ZIP)
+# - TODO: calculate residuals? more error calculations
+# - TODO: check sample size (ok to have only 36 records?)
+# - TODO: improve predictions and MSPE
+# - TODO: see other lists
 
-#################################################################################################
-
-
-#################################################################################################
-# NOTE: Currently trying to solve convergence issue -  this is part of that
-
-# deal with convergence warnings by ignoring for now - still gives different results every time, no good
-#import warnings
-#from statsmodels.tools.sm_exceptions import ConvergenceWarning
-#warnings.simplefilter('ignore', ConvergenceWarning)
 #################################################################################################
 
 # Read in data
 lmdata = pd.read_csv("field_data.csv")
+print(lmdata)
 
 # replace first header in csv (need second header to pull our needed columns)
 new_header = lmdata.iloc[0]
@@ -49,7 +45,7 @@ lmdata.columns = new_header
 lmdata = lmdata.drop(labels=157, axis=0)
 
 # select the columns we need (note: the csv file has an extra space at the end of the temp string)
-lmdata_reduced = lmdata[["MONTH", "YEAR", "SALINITY (%)", "TEMPERATURE (C) ", "CHLOROPHYTES (ug/l)"]]
+lmdata_reduced = lmdata[["MONTH", "YEAR", "LAKE VOLUME (m^3)", "SALINITY (%)", "TEMPERATURE (C) ", "CHLOROPHYTES (ug/l)"]]
 
 # remove missing values (all denoted by 99999)
 lmdata_filter1 = lmdata_reduced[lmdata_reduced["SALINITY (%)"] != '"99999"']
@@ -70,33 +66,18 @@ lmdata["SALINITY (%)"] = lmdata["SALINITY (%)"].astype(float)
 lmdata["TEMPERATURE (C) "] = lmdata["TEMPERATURE (C) "].astype(float)
 lmdata["CHLOROPHYTES (ug/l)"] = lmdata["CHLOROPHYTES (ug/l)"].astype(float)
 
-#################################################################################################
-# NOTE: This is for MLR model only and is not accurate but will be used for presentation!
-
-# try dropping entries with zero chlorophytes:
-#lmdata = lmdata[~(lmdata['CHLOROPHYTES (ug/l)'] == 0)]
-
-# need to re-index this for regression
-#lmdata = lmdata.reset_index()
-
-# drop extra index column from re-indexing
-#lmdata = lmdata.drop(labels="index", axis=1)
-#################################################################################################
+#print(lmdata)
 
 # assign predictor (X) and response (Y) variables for regression - note that X is an array
 X = lmdata[["SALINITY (%)", "TEMPERATURE (C) "]]
 Y = lmdata["CHLOROPHYTES (ug/l)"]
 
-#################################################################################################
-# NOTE: This is for MLR model only!!
-# log transform the chlorophytes variable since it has a lot of small values near 0
-#Y = np.log(Y) # note that this is the natural log
-#################################################################################################
+# set seed to control for randomness
+random.seed(7)
 
 # split data into train and test sets:
-mask = np.random.rand(len(lmdata)) < 0.5 # approx random 50/50 split (30 train, 28 test)
-df_train = lmdata[mask]
-df_test = lmdata[~mask]
+df_train = lmdata.sample(frac = 0.5, random_state=123)
+df_test = lmdata.drop(df_train.index)
 #print('Training data set length='+str(len(df_train)))
 #print('Testing data set length='+str(len(df_test)))
 #print(df_train)
@@ -133,17 +114,31 @@ y_test, X_test = dmatrices(expr_test, df_test, return_type='dataframe')
 
 # run the regression using sklearn
 regr = linear_model.LinearRegression()
-model = regr.fit(X_train, y_train) # use just X,Y to get values from abstract (no data splitting) - but predictions in
-                                   # abstract are still viable (this is just better)
-fitted = model.predict(X_train) # these are the chlorophyte values predicted from/fitted to the regression model
+#model = regr.fit(X_train, y_train) # use for prediction
+model = regr.fit(X, Y) # use to run whole dataset
+#fitted = model.predict(X_train) # these are the chlorophyte values predicted from/fitted to the regression model (prediction values)
+fitted = model.predict(X) # whole dataset values
 # print(fitted)
 
 # predict new values (23.5% salinity, 28.5 deg temp) -> this is a 2.5 deg C and 7.5% NaCl increase
-new_x = {'Intercept': [1], 'x1_tr': [23.5], 'x2_tr': [28.5]}
-new_x = pd.DataFrame(data=new_x)
-y_pred = model.predict(new_x)
+#new_x = {'Intercept': [1], 'x1_tr': [23.5], 'x2_tr': [28.5]}
+#new_x = pd.DataFrame(data=new_x)
+#y_pred = model.predict(new_x)
 #print("MLR prediction:")
 #print(y_pred)
+
+# print p values
+#params = np.append(regr.intercept_,regr.coef_)
+#predictions = regr.predict(X)
+#new_X = np.append(np.ones((len(X),1)), X, axis=1)
+#M_S_E = (sum((Y-predictions)**2))/(len(new_X)-len(new_X[0]))
+#v_b = M_S_E*(np.linalg.inv(np.dot(new_X.T,new_X)).diagonal())
+#s_b = np.sqrt(v_b)
+#t_b = params/ s_b
+#p_val =[2*(1-sp.stats.t.cdf(np.abs(i),(len(new_X)-len(new_X[0])))) for i in t_b]
+#p_val = np.round(p_val,3)
+#print("MLR p values")
+#print(p_val)
 
 # print parameters/coefficients
 #print("MLR coefficients")
@@ -157,28 +152,63 @@ y_pred = model.predict(new_x)
 #print("MLR R^2")
 #print(model.score(X, Y)) # R^2 is about 0.44 for MLR with zeros removed
 
+# compute root mean squared prediction error
+#X_test = X_test.rename(columns={"x1_te": "x1_tr", "x2_te": "x2_tr"}) # update names for prediction (must comment out for ZIP)
+#mlr_predictions = model.predict(X_test)
+#predicted_counts_mlr=np.round(mlr_predictions)
+#actual_counts_mlr = y_test["y_te"]
+#actual_counts_mlr = actual_counts_mlr.to_frame()
+#print('MLR RMSE='+str(np.sqrt(np.sum(np.power(np.subtract(predicted_counts_mlr,actual_counts_mlr),2)))))
+
 # plot X vs Y (these are just values from the actual data)
-#plt.scatter(X["SALINITY (%)"], Y) # salinity and chlorophytes
-#plt.show()
-#plt.scatter(X["TEMPERATURE (C) "], Y) # temp and chlorophytes
-#plt.show()
+a, b = np.polyfit(X["SALINITY (%)"], Y, 1)
+c, d = np.polyfit(X["TEMPERATURE (C) "], Y, 1)
+plt.scatter(X["SALINITY (%)"], Y) # salinity and chlorophytes
+plt.plot(X["SALINITY (%)"], a*X["SALINITY (%)"]+b)
+plt.title("Chlorophyte Concentration (Actual) vs. Salinity")
+plt.xlabel("Salinity (% NaCl)")
+plt.ylabel("Chlorophyte Concentration ($\mu$g/L)")
+plt.show()
+plt.scatter(X["TEMPERATURE (C) "], Y) # temp and chlorophytes
+plt.plot(X["TEMPERATURE (C) "], c*X["TEMPERATURE (C) "]+d)
+plt.title("Chlorophyte Concentration (Actual) vs. Temperature")
+plt.xlabel("Temperature ($^\circ$C)")
+plt.ylabel("Chlorophyte Concentration ($\mu$g/L)")
+plt.show()
 
 # plot X vs fitted (shows relation between predicted chloro values and predictors)
-#plt.scatter(X["SALINITY (%)"], fitted)
-#plt.show()
-#plt.scatter(X["TEMPERATURE (C) "], fitted)
-#plt.show()
+e, f = np.polyfit(X["SALINITY (%)"], fitted, 1)
+g, h = np.polyfit(X["TEMPERATURE (C) "], fitted, 1)
+plt.plot(X["SALINITY (%)"], e*X["SALINITY (%)"]+f)
+plt.title("Chlorophyte Concentration (Fitted) vs. Salinity")
+plt.xlabel("Salinity (% NaCl)")
+plt.ylabel("Chlorophyte Concentration ($\mu$g/L)")
+plt.scatter(X["SALINITY (%)"], fitted)
+plt.show()
+plt.scatter(X["TEMPERATURE (C) "], fitted)
+plt.plot(X["TEMPERATURE (C) "], g*X["TEMPERATURE (C) "]+h)
+plt.title("Chlorophyte Concentration (Fitted) vs. Temperature")
+plt.xlabel("Temperature ($^\circ$C)")
+plt.ylabel("Chlorophyte Concentration ($\mu$g/L)")
+plt.show()
 
 # plot actual vs fitted (should be tightly fit around line y=x)
-#plt.scatter(Y, fitted)
-#plt.show()
+plt.scatter(Y, fitted)
+plt.plot(Y, Y)
+plt.title("Fitted vs. Actual Chlorophyte Concentration")
+plt.xlabel("Actual Chlorophyte Concentration ($\mu$g/L)")
+plt.ylabel("Fitted Chlorophyte Concentration ($\mu$g/L)")
+plt.show()
 
 # plot temp vs salinity (proxy test for correlation, shows these are related as we know in theory
                         # - should not affect prediction, just inference)
-#plt.scatter(X["SALINITY (%)"], X["TEMPERATURE (C) "])
-#plt.xlabel("Salinity (%)")
-#plt.ylabel("Temperature (C)")
-#plt.show()
+m, n = np.polyfit(X["SALINITY (%)"], X["TEMPERATURE (C) "], 1)
+plt.scatter(X["SALINITY (%)"], X["TEMPERATURE (C) "])
+plt.plot(X["SALINITY (%)"], m*X["SALINITY (%)"]+n)
+plt.title("Temperature vs. Salinity")
+plt.xlabel("Salinity (% NaCl)")
+plt.ylabel("Temperature ($^\circ$C)")
+plt.show()
 
 # NOTE: See Rachel's code for 3D MLR plot(s)!!!
 
@@ -188,19 +218,10 @@ y_pred = model.predict(new_x)
 
 # do zero-inflated Poisson (ZIP) model (rate model with chlorophytes as rate):
 
-#################################################################################################
-# NOTE: trying to deal with convergence/ZIP fit problem by checking if not enough zero inflation
-
-# check if zero inflation is real - currently doesn't work without statsmodels version 0.14 in dev
-#mod = statsmodels.discrete.discrete_model.Poisson.from_formula(expr_train, df_train)
-#res = mod.fit()
-#diag = res.get_diagnostic()
-#diag.test_poisson_zeroinflation().pvalue
-#################################################################################################
-
 # Run ZIP model
 zip_training_results = sm.ZeroInflatedPoisson(endog=y_train, exog=X_train, exog_infl=X_train, inflation='logit').fit()
 print(zip_training_results.summary())
+#print(zip_training_results.__dir__())
 
 # calculuate root mean square error (it's really root mean squared prediction error)
 zip_predictions = zip_training_results.predict(X_test,exog_infl=X_test)
@@ -208,19 +229,30 @@ predicted_counts=np.round(zip_predictions)
 actual_counts = y_test["y_te"]
 print('ZIP RMSE='+str(np.sqrt(np.sum(np.power(np.subtract(predicted_counts,actual_counts),2)))))
 
-# prediction plot
-fig = plt.figure()
-fig.suptitle('Predicted versus actual counts using the ZIP model')
-predicted, = plt.plot(X_test.index, predicted_counts, 'go-', label='Predicted')
-actual, = plt.plot(X_test.index, actual_counts, 'ro-', label='Actual')
-plt.legend(handles=[predicted, actual])
+# prediction plot (original with indices on x axis)
+#fig = plt.figure()
+#fig.suptitle('Predicted vs. Actual Chlorophyte Concentrations')
+#predicted, = plt.plot(X_test.index, predicted_counts, 'go-', label='Predicted')
+#actual, = plt.plot(X_test.index, actual_counts, 'ro-', label='Actual')
+#plt.xlabel("Indices")
+#plt.ylabel("Chlorophyte Concentration ($\mu$g/L)")
+#plt.legend(handles=[predicted, actual])
+#plt.show()
+
+# prediction plot (bubble plot)
+diff = np.absolute(actual_counts - predicted_counts)
+test_data = {'Temperature ($^\circ$C)': X_test["x2_te"], 'Salinity (% NaCl)': X_test["x1_te"], 'Difference': diff}
+test_data_df = pd.DataFrame(data=test_data)
+#print(test_data_df)
+sns.scatterplot(data=test_data_df, x="Temperature ($^\circ$C)", y="Salinity (% NaCl)", size="Difference", legend=False, sizes=(20, 2000))
+plt.title("Predicted vs. Actual Chlorophyte Concentration")
 plt.show()
 
 # manually run predictions for particular values (note that param's 3 through 6 indices are intercept, salinity, temp)
 # since ZIP is not converging, run this 3 times and take the average of the outputs
-pred_chloro = np.exp(np.matmul([1, 23.5, 28.5], zip_training_results.params[3:6]))
-print("ZIP prediction")
-print(pred_chloro)
+#pred_chloro = np.exp(np.matmul([1, 23.5, 28.5], zip_training_results.params[3:6]))
+#print("ZIP prediction")
+#print(pred_chloro)
 
 #################################################################################################
 # NOTE: This is an attempt to get the predict method to work for the ZIP model
@@ -237,13 +269,3 @@ print(pred_chloro)
 #future_preds = np.round(specific_pred)
 #print(future_preds)
 #################################################################################################
-
-# TODO: check sample size (ok to have only 36 records?)
-
-# TODO: run diagnostic plots
-
-# TODO: 3D plots of MLR
-
-# TODO: predicting, calculuate MSPE, etc
-
-#TODO: calculate errors/residuals (distance from plane)
